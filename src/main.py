5fe2049f96d4e7d4a75cfe80abd94c19bdbbf931
Anders Wiberg Olsen <anders@wiberg.tech>
2022-04-02 20:00:53 +0200

    Ensure download works properly

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ import clipboard
 import youtube_dl
 import hashlib
 from dataclasses import dataclass
+import requests
+from bs4 import BeautifulSoup
 
 
 VIDEO_PATH = '../videos/'
@@ -22,13 +24,41 @@ class Video:
         return hashlib.sha256(self.url.encode()).hexdigest()
 
     def filename(self) -> str:
-        return f'{self.url_hash()}.{self.extension}'
+        return f'{self.url_hash()}__{self.title}.{self.extension}'
 
 def get_clipboard_content():
     return clipboard.paste()
 
 def is_url(s):
     return 'https://' in s
+
+def try_get_alternative_video_url(url):
+    # print(url)
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, 'html.parser')
+    iframes = soup.select('div.responsive-player > iframe[src]')
+    if iframes is None or len(iframes) == 0:
+        return None
+    meta_title = soup.findAll('meta', {'itemprop': 'name'})
+    title = None
+    if meta_title is not None and len(meta_title) > 0:
+        title = meta_title[0]['content']
+        print('title is', title)
+    player_url = iframes[0]['src']
+    # print(player_url)
+
+    player_request = requests.get(player_url)
+    player_soup = BeautifulSoup(player_request.text, 'html.parser')
+    video_sources = player_soup.select('video#video > source')
+    print('found sources:')
+    for video_source in video_sources:
+        print('-', video_source['title'], video_source['src'])
+
+    alternative_url = video_sources[0]['src']
+    alternative_url_size = video_sources[0]['title']
+    # print(alternative_url)
+    return alternative_url, title, alternative_url_size
+
 
 def download_video(url):
     def is_video_supported(url):
@@ -47,11 +77,18 @@ def download_video(url):
     }
 
     ydl = youtube_dl.YoutubeDL(options)
+    alternative_title = None
     with ydl:
         try:
             result = ydl.extract_info(url, download=False)
         except youtube_dl.utils.DownloadError:
-            raise DownloadFailedException(url)
+            alternative_url, alternative_title, size = try_get_alternative_video_url(url)
+            if alternative_url is None:
+                raise DownloadFailedException(url)
+
+            print('alternative url', alternative_url)
+            result = ydl.extract_info(alternative_url, download=False)
+            url = alternative_url
         except youtube_dl.utils.UnsupportedError:
             raise DownloadFailedException(url)
         except Exception as e:
@@ -65,8 +102,15 @@ def download_video(url):
 
     title = video['title']
     extension = video['ext']
-    video = Video(url, video['title'], video['ext'])
+
+    if alternative_title is not None:
+        print('changing title')
+        title = alternative_title
+    video = Video(url, title, extension)
+
     return video
+
+videos_to_download = []
 
 def listen_for_changes(sleep_amount = 1):
     print('Listening for clipboard content...')
@@ -88,17 +132,30 @@ def listen_for_changes(sleep_amount = 1):
             print(f'New content: {content}')
             try:
                 video = download_video(content)
-                print(video.filename())
+                print('title is', video.title)
+                print('filename is', video.filename())
+                print('download url is', video.url)
+                videos_to_download.append(video)
             except DownloadFailedException as e:
                 print(f'Failed to download "{(e.download_url)}"')
         time.sleep(sleep_amount)
 
 def main():
+    # .responsive-player > iframe
     listen_for_changes()
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
+        for video in videos_to_download:
+            print('will now download', video.title, 'from', video.url)
+            print(video)
+            options = {
+                # 'quiet': True,
+                'outtmpl': f'videos/{video.filename()}'
+            }
+            ydl = youtube_dl.YoutubeDL(options)
+            ydl.download([video.url])
         sys.exit()
 
